502f8264336f05f235fe0643ca9b23a2a70c4d57
bOQPCZVRpMMRSVHE <E.Premium@proton.me>
2026-02-02 03:20:30 -0500

    Update GKCx2.lua

--- GKCx2.lua
+++ GKCx2.lua
@@ -1,5 +1,5 @@
 return {
     ['bwbabla%a_akaaa%a`bMa_aA'] = 'bwa/bsa&a%ahafaya>bOa^aubha,baakasbxa>bl',
     ['bwbabla%a_akacaya`bMa_aA'] = 'bwbabsa*a%amafaza>bMa^aubha:baab',
-    ['bwbabla%a_akabava`bH'] = 'bwbgbsa<a%akafaya>bJa^avbha,baacasbm',
+    ['bwbabla%a_akabava`bH'] = 'bwbgbsa<a%akafaya>bJa^avbha,baacasbm'
 }
